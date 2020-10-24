4e18c6993d04fa1ef8bcbd9f2595db2fca6b0d03
Adam <adamfrew1999@gmail.com>
2020-10-24 03:20:32 +0100

    Wider bandstop filter, cleaner signal

--- ecg_filter.py
+++ ecg_filter.py
@@ -12,23 +12,35 @@ tplot = plt.plot(data)
 
 n = np.arange(-200, 200+1)
 
-#Impulse response of a band stop filter between 45Hz and 55Hz https://youtu.be/Z0Vxuo1c5yY?t=1627
-h = (1/(n*np.pi)) * (np.sin(45/500*2*np.pi*n) - np.sin(55/500*2*np.pi*n))
+#Impulse response of a band stop filter between 20Hz and 120Hz https://youtu.be/Z0Vxuo1c5yY?t=1627
+h = (1/(n*np.pi)) * (np.sin(20/500*2*np.pi*n) - np.sin(120/500*2*np.pi*n))
 #L'hopitals rule - to avoid diving by 0 causing NaN error
 h[200] = 1 - (55/500*2*np.pi - 45/500*2*np.pi)/np.pi
 
 plt.figure(2)
 tplot = plt.plot(h)
-'''
+
+dataf = np.fft.fft(data)
+#Convert FFT data to decibels relative to full scale (dBFS)
+datafdB = 20*np.log10(abs(dataf)*2/len(dataf)/(pow(2,15)-1))
+
+#Create frequency axis
+f = np.linspace(0, fs, len(dataf))
+
+#Plot sample in frequency domain
+plt.figure(3)
+fplot = plt.plot(f, datafdB)
+fplot = plt.xlabel("Frequency (Hz)")
+fplot = plt.ylabel("dBFS")
+plt.xscale("log")
+
+
 filter = fir.FIR_filter(h)
 for i in range(len(data)):
     data[i] = filter.dofilter(data[i])
 
-plt.figure(3)
-tplot = plt.plot(data)
-'''
 plt.figure(4)
-y = signal.lfilter(h,1,data)
-tplot = plt.plot(y)
+tplot = plt.plot(data)
+
 
 plt.show()
