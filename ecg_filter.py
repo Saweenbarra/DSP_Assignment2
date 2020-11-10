282e5ec7ad625a0058b8e8b83e82f8c495711e71
Saweenbarra <adamfrew1999@gmail.com>
2020-11-10 22:41:53 +0000

    merge

--- ecg_filter.py
+++ ecg_filter.py
@@ -24,22 +24,21 @@ plt.xscale("log")
 plt.yscale("log")
 
 M = fs*2
+y = np.ones(M)
 
-#Bandstop markers
-
+#Indexes corresponding to 45Hz and 55Hz
 k1 = int(45/fs * M)
 k2 = int(55/fs * M)
 
-#Filter function before bandstop
-y = np.ones(M)
-
-#Filter function with bandstop
+#Set range of 45Hz to 55Hz to 0, and nyquist mirror image
 y[k1:k2+1] = 0
 y[M-k2:M-k1+1] = 0
 
+#Indexes corresponding to 0Hz and 0.5Hz
 k1 = int(0/fs * M)
 k2 = int(0.5/fs * M)
 
+#Set range of 0Hz to 0.5Hz to 0, and nyquist mirror image
 y[k1:k2+1] = 0
 y[M-k2:M-k1+1] = 0
 
@@ -48,10 +47,11 @@ plt.figure(3)
 plt.plot(ftransfer, y)
 plt.xlabel("Frequency (Hz)")
 
+#Take FFT of transfer function
 y = np.fft.ifft(y)
 y = np.real(y)
 
-
+#Swap FFT around data to obtain impulse response
 h = np.zeros(M)
 h[0:int(M/2)] = y[int(M/2):M]
 h[int(M/2):M] = y[0:int(M/2)]
